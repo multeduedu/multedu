ab2271b0644f9db690701c8abbda522734f9a423
Rodrigo Rizzo <38816338+rodrigorizzo@users.noreply.github.com>
2026-03-15 11:17:11 -0300

    fix/ url de todos os vídeos explicativos estão funcionando

--- src/data/multiplicadores.ts
+++ src/data/multiplicadores.ts
@@ -13,7 +13,7 @@ export const DadosMultiplicadores: MultiplicadorData[] = [
 multiplicador: 2,
 descricao: "",
 regraMetade: false,
-videoUrl: "https://www.youtube.com/watch?v=XPylUHjQgxg",
+videoUrl: "XPylUHjQgxg",
 helpText:{
 1: "Dobrar o 1º valor e colocar na caixa.",
 2: "Dobrar o 2º valor e colocar na caixa.",
@@ -26,7 +26,7 @@ helpText:{
 multiplicador: 3,
 descricao: "A multiplicação por 3 é semelhante à por 8, exceto que agora pegamos a metade do vizinho e, se for ímpar, acionamos +5.",
 regraMetade: false,
-videoUrl: "https://www.youtube.com/watch?v=9rsbrrk8gSI",
+videoUrl: "9rsbrrk8gSI",
 helpText:{
 1: "Subtraia por -10 e dobre o valor e, se for ímpar, adicione +5.",
 2: "Subtraia por -9, em seguida dobre o valor e adicione metade do vizinho. Se for ímpar, adicione +5.",
@@ -39,7 +39,7 @@ helpText:{
 multiplicador: 4,
 descricao: "A multiplicação por 4 é semeslhante à por 9, exceto que agora pegamos a metade do vizinho.",
 regraMetade: false,
-videoUrl: "https://www.youtube.com/watch?",
+videoUrl: "9rsbrrk8gSI",
 helpText:{
 1: "Subtraia por -10 e, se ímpar, adicione +5.",
 2: "Subtraia por -9 e adicione a metade do vizinho e, se ímpar, adicione +5.",
@@ -52,7 +52,7 @@ helpText:{
 multiplicador: 5,
 descricao: "Se for ímpar adicionamos 5 e mais metade do vizinho.",
 regraMetade: true,
-videoUrl: "https://www.youtube.com/watch?v=t5Tj47vdn9Y",
+videoUrl: "t5Tj47vdn9Y",
 helpText:{
 1: "Se for par o número é 0, se ímpar o número é 5.",
 2: "Se ímpar somamos +5 e adicionamos metade do vizinho.",
@@ -65,7 +65,7 @@ helpText:{
 multiplicador: 6,
 descricao: "",
 regraMetade: true,
-videoUrl: "https://www.youtube.com/watch?v=Mj1kefQltyU",
+videoUrl: "Mj1kefQltyU",
 helpText:{
 1: "Mantenha o 1° dígito se par. Se for ímpar, adicione +5.",
 2: "Some o 2° dígito com a metade do vizinho à direita. Se for ímpar, adicione +5.",
@@ -79,7 +79,7 @@ helpText:{
 multiplicador: 7,
 descricao: "Dobre o número e some metade do vizinho; adicione 5 se o número for ímpar.",
 regraMetade: false,
-videoUrl: "https://www.youtube.com/watch?v=4uIW08M3xQU",
+videoUrl: "4uIW08M3xQU",
 helpText:{
 1: "Dobrar o 1° dígito se for par. Se for ímpar, adicione +5 ao resultado.",
 2: "Dobrar o 2° dígito e somar metade do vizinho à direita. Se for ímpar, adicione +5.",
@@ -92,7 +92,7 @@ helpText:{
 multiplicador: 8,
 descricao: "A multiplicação por 8 é semelhante à por 9, exceto que há duplicação.",
 regraMetade: false,
-videoUrl: "https://www.youtube.com/watch?v=Mlfwvv-kFu4",
+videoUrl: "Mlfwvv-kFu4",
 helpText:{
 1: "Subtraia por -10 e dobre.",
 2: "Substraia por -9 e dobre o valor. Em seguida, adicione o vizinho e, se for maior que 9, coloque um ponto e adicione +1 ao vizinho.",
@@ -105,7 +105,7 @@ helpText:{
 multiplicador: 9,
 descricao: "O primeiro dígito subtraimos por -10. Já os demais subtraimos por -9 e, no último dígito, olhamos para o vizinho para saber se subtraimos por -1.",
 regraMetade: false,
-videoUrl: "https://www.youtube.com/watch?v=UpXQoi_9kCs",
+videoUrl: "UpXQoi_9kCs",
 helpText:{
 1: "Subtraia por -10.",
 2: "Subtraia por -9 e adicione ao vizinho.",
